69e04543d009e3f078213f7ba8e6b616bc42644f
varondel <arondelv@gmail.com>
2018-07-23 18:31:03 +0200

    Login

---
+++ src/components/CreateRecipe.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+   
+    render() {
+  
+      return (
+      <div>
+          Create recipe form
+      </div>
+      );
+    }
+  }
+  
+  export default Login;
+  
